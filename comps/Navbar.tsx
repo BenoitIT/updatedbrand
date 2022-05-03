e1be1c2aed0df05a3bac9623a8665a5baa534db8
mansurissa <mansurissa6@gmail.com>
2022-05-03 23:27:08 +0200

    nav bar responsiveness

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import Image from 'next/image';
 import Home from '/public/resources/icons/home.svg';
 import Skills from '/public/resources/icons/skills.svg';
@@ -13,15 +13,6 @@ import Link from 'next/link';
 
 const SideNav: FC = () => {
   const [responsive, setResponsive] = useState('');
-  useEffect(() => {
-    function handleResize() {
-      setResponsive(window.innerWidth < 1024 ? 'tab' : '');
-    }
-
-    window.addEventListener('resize', handleResize);
-  });
-
-  console.log('NNNNNN', responsive);
 
   return (
     <>
@@ -31,9 +22,27 @@ const SideNav: FC = () => {
             <div className={`${styles.logo} logo`}>
               <Image src={Logo} />
             </div>
-          </Link>
-          <div className={`${styles.burger} burger`}>
-            <Image src={Burger} />
+          </Link>{' '}
+          <div className={styles.responsiveController}>
+            {responsive === 'hidden' ? (
+              <div
+                className={`${styles.burger} burger`}
+                onClick={() => {
+                  setResponsive('');
+                }}
+              >
+                <Image src={Burger} />
+              </div>
+            ) : (
+              <div
+                className={styles.closeBtn}
+                onClick={() => {
+                  setResponsive('hidden');
+                }}
+              >
+                X
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -95,51 +104,86 @@ const SideNav: FC = () => {
       </div>
 
       {/* MOBILE NAV */}
-      <div className={styles.sideNavMob}>
+      <div className={`${styles.sideNavMob} ${responsive}`}>
         <div>
           <div className={styles.navItems}>
             <Link href='/'>
-              <div className={`${styles.navItem} ${styles.active}`}>
+              <div
+                className={`${styles.navItem} ${styles.active}`}
+                onClick={() => {
+                  setResponsive('hidden');
+                }}
+              >
                 <div className={`${styles.navIcon}`}>
                   <Image src={Home} />
                 </div>
                 <p>Home</p>
               </div>
             </Link>
+
             <Link href='/#about'>
-              <div className={`${styles.navItem}`}>
+              <div
+                className={`${styles.navItem}`}
+                onClick={() => {
+                  setResponsive('hidden');
+                }}
+              >
                 <div className={`${styles.navIcon}`}>
                   <Image src={Skills} />
                 </div>
                 <p>Skills</p>
               </div>
             </Link>
+
             <Link href='/#portfolio'>
-              <div className={`${styles.navItem}`}>
+              <div
+                className={`${styles.navItem}`}
+                onClick={() => {
+                  setResponsive('hidden');
+                }}
+              >
                 <div className={`${styles.navIcon}`}>
                   <Image src={Portfolio} />
                 </div>
                 <p>Portifolio</p>
               </div>
             </Link>
+
             <Link href='/#contacts'>
-              <div className={`${styles.navItem}`}>
+              <div
+                className={`${styles.navItem}`}
+                onClick={() => {
+                  setResponsive('hidden');
+                }}
+              >
                 <div className={`${styles.navIcon}`}>
                   <Image src={Message} />
                 </div>
                 <p>Contacts</p>
               </div>
             </Link>
+
             <Link href='/resume'>
-              <div className={`${styles.navItem}`}>
+              <div
+                className={`${styles.navItem}`}
+                onClick={() => {
+                  setResponsive('hidden');
+                }}
+              >
                 <div className={`${styles.navIcon}`}>
                   <Image src={Cv} />
                 </div>
                 <p>Resume</p>
               </div>
             </Link>
+
             <a href='https://devissa.com/' target='_blank'>
-              <div className={`${styles.navItem}`}>
+              <div
+                className={`${styles.navItem}`}
+                onClick={() => {
+                  setResponsive('hidden');
+                }}
+              >
                 <div className={`${styles.navIcon}`}>
                   <Image src={Blog} />
                 </div>
